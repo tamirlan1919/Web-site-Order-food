039de0c0441bbfda7cf5a31a9479d0ed66e0c24f
tamirlan1919 <tchinchaev@bk.ru>
2022-09-25 17:23:34 +0300

    Web site oreder food

---
+++ functions.js
@@ -0,0 +1,7 @@
+function download(){
+	alert("This app has been removed from the play market for Russia!")
+}
+
+function register(){
+	alert("You do not have reataurant! 😈")
+}
---
+++ js/functions.js
@@ -0,0 +1,15 @@
+function download(){
+	alert("This app has been removed from the play market for Russia!")
+}
+
+function register(){
+	alert("You do not have reataurant! 😈")
+}
+
+function visible_phone_panel(){
+	document.getElementById("phone_panel").classList.remove("invisible");
+}
+
+function invisible_phone_panel(){
+	document.getElementById("phone_panel").classList.add("invisible");
+}
